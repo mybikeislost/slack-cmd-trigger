ec0099ff4366bdee852cfe2dc98c39ac3644a6f8
Brian Willard <mybikeislost@gmail.com>
2019-10-12 18:22:02 -0500

    Update index.py

--- index.py
+++ index.py
@@ -1,34 +1,58 @@
+from __future__ import print_function
+from datetime import date
+from datetime import timedelta
 import argparse
 from slacker import Slacker
 
-settings = None
+
+# generate a legacy token at https://api.slack.com/custom-integrations/legacy-tokens
+apiKey = "LEGACY_API_KEY_HERE"
+
+channels = [
+     "discuss-storage",
+     "discuss-workstations"
+     ]
+
+slashCmd = "clip"
+
+days = 2 #default number of days to clip. 2 sets it to yesterday
 
 def init_settings():
+	global days
+	#days = 2 # default to only clipping yesterday.
+
 	parser = argparse.ArgumentParser()
-
-	parser.add_argument('-t', '--token', help='Slack API token', required=True, type=str)
-	parser.add_argument('-c', '--channel', help='Slack channel', required=True, type=str)
-	parser.add_argument('-cmd', '--command', help='Slack command', required=True, type=str)
-	parser.add_argument('-i', '--instruction', help='Slack instruction for command', required=True, type=str)
-
+	parser.add_argument('-w','--week', help='clip the previous 7 days.', nargs='?', const=1, default=False)
+	parser.add_argument('-m','--month', help='clip the prior 31 days', nargs='?', const=1, default=False)
 	args = parser.parse_args()
+
+	# set the days to a weekly or monthly lookback, default to yesterday with no args.
+	if args.week:
+ 		days = 8
+	if args.month:
+ 		days = 32
 	return args
 
 def main():
 	global settings
-   
+	today = date.today()
 	try:
 		settings = init_settings()
-		slack = Slacker(settings.token)
-		channel_id = slack.channels.get_channel_id(settings.channel)
-		slack.chat.command(
-	        channel=channel_id,
-	        command='/' + settings.command,
-	        text=settings.instruction
-		)
-		
+		slack = Slacker(apiKey)
+		for c in channels:
+			print("#"+c)
+			for i in range(1,days): #for the prior 7 days use (1,8), to include today use (0,7)...
+				clipDate =  (today - timedelta(days=i)).strftime("%m/%d/%Y")
+				print("\tclipping:", clipDate)				
+				channel_id = slack.channels.get_channel_id(c)
+				slack.chat.command(
+                    channel=channel_id,
+                    command='/' + slashCmd,
+                    text=clipDate
+				)
+			
 	except (KeyboardInterrupt, SystemExit):
-		print "-> Aborted through user interaction"
+		print("-> Aborted through user interaction")
 
 
 if __name__ == "__main__":
